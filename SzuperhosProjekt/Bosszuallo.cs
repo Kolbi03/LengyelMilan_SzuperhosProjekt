3864f59b5c05c2a261905dee7f74f718971baaca
Lengyel Milán <lengyel.milan.hu@gmail.com>
2023-10-29 20:05:19 +0100

    Final

---
+++ SzuperhosProjekt/Bosszuallo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzuperhosProjekt
+{
+    public abstract class Bosszuallo : ISzuperhos
+    {
+        private double szuperero;
+        private bool vanEGyengesege;
+
+        protected Bosszuallo(double szuperero, bool vanEGyengesege)
+        {
+            this.szuperero = szuperero;
+            this.vanEGyengesege = vanEGyengesege;
+        }
+
+        public double Szuperero { get => szuperero; set => szuperero = value; }
+        public bool VanEGyengesege { get => vanEGyengesege; set => vanEGyengesege = value; }
+
+        public bool LegyoziE(ISzuperhos Szuperhos)
+        {
+            if (Szuperhos.GetType() == typeof(Bosszuallo))
+            {
+                Bosszuallo sz = (Bosszuallo)Szuperhos;
+                if (Szuperhos.MekkoraAzEreje() < this.szuperero && sz.VanEGyengesege)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (this.szuperero >= Szuperhos.MekkoraAzEreje() * 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double MekkoraAzEreje()
+        {
+            return szuperero;
+        }
+
+
+        public abstract bool MegmentiAVilagot();
+
+        public override string ToString()
+        {
+            return $"Szupererő: {this.szuperero}; {(this.vanEGyengesege ? "van gyengesége" : "nincs gyengesége")}";
+        }
+    }
+}
---
+++ SzuperhosProjekt/Vasember.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzuperhosProjekt
+{
+    public class Vasember : Bosszuallo, IMilliardos
+    {
+        private static Random rnd = new Random();
+
+        public Vasember() : base(200, true)
+        {
+
+        }
+
+        public void KutyutKeszit()
+        {
+            double plus = rnd.NextDouble() * 10;
+            this.Szuperero += plus;
+        }
+
+        public override bool MegmentiAVilagot()
+        {
+            return this.Szuperero > 1000;
+        }
+
+        public override string ToString()
+        {
+            return $"Vasember: {base.ToString()}";
+        }
+    }
+}
